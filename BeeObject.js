997a1a3dccdbe1e284250775bc8517a8ae33d490
Batatia, Hadj <hb149@hw.ac.uk>
2020-11-03 23:14:15 +0400

    class S6

--- BeeObject.js
+++ BeeObject.js
@@ -1,58 +1,64 @@
-function Bee(beeNumber) {
-    //the HTML element corresponding to the IMG of the wolf
-    this.htmlElement = createBeeImg(beeNumber);
-    //iits HTML ID
-    this.id = this.htmlElement.id;
-    //the left position (x)
-    this.x = this.htmlElement.offsetLeft;
-    //the top position (y)
-    this.y = this.htmlElement.offsetTop;
+class Bee {
+    constructor(beeNumber) {
+        //the HTML element corresponding to the IMG of the wolf
+        this.htmlElement = createBeeImg(beeNumber);
+        //iits HTML ID
+        this.id = this.htmlElement.id;
+        //the left position (x)
+        this.x = this.htmlElement.offsetLeft;
+        //the top position (y)
+        this.y = this.htmlElement.offsetTop;
 
-    this.move = function(dx, dy) {
-        //move the bees by dx, dy
-        this.x += dx;
-        this.y += dy;
-        this.display();
+        this.move = function (dx, dy) {
+            //move the bees by dx, dy
+            this.x += dx;
+            this.y += dy;
+            this.display();
 
-    };
+        };
 
-    this.display = function() {
-        //ajust position of bee and display it
-        this.fitBounds();
-        this.htmlElement.style.left = this.x + "px";
-        this.htmlElement.style.top = this.y + "px";
-        this.htmlElement.style.display = "block";
-    };
+        this.display = function () {
+            //ajust position of bee and display it
+            this.fitBounds();
+            this.htmlElement.style.left = this.x + "px";
+            this.htmlElement.style.top = this.y + "px";
+            this.htmlElement.style.display = "block";
+        };
 
-    this.fitBounds = function() {
-        //check and make sure the bees stays in the board space
-        let parent = this.htmlElement.parentElement;
-        let iw = this.htmlElement.offsetWidth;
-        let ih = this.htmlElement.offsetHeight;
-        let l = parent.offsetLeft;
-        let t = parent.offsetTop;
-        let w = parent.offsetWidth;
-        let h = parent.offsetHeight;
-        if (this.x < 0) this.x = 0;
-        if (this.x > w - iw) this.x = w - iw;
-        if (this.y < 0) this.y = 0;
-        if (this.y > h - ih) this.y = h - ih;
+        this.fitBounds = function () {
+            //check and make sure the bees stays in the board space
+            let parent = this.htmlElement.parentElement;
+            let iw = this.htmlElement.offsetWidth;
+            let ih = this.htmlElement.offsetHeight;
+            let l = parent.offsetLeft;
+            let t = parent.offsetTop;
+            let w = parent.offsetWidth;
+            let h = parent.offsetHeight;
+            if (this.x < 0)
+                this.x = 0;
+            if (this.x > w - iw)
+                this.x = w - iw;
+            if (this.y < 0)
+                this.y = 0;
+            if (this.y > h - ih)
+                this.y = h - ih;
 
-    };
+        };
 
-    this.remove = function() {
-        //remove the bee and delete its IMG
-        if (typeof bees !== 'undefined') {
-            //remove element from bees array
-            const index = bees.indexOf(this);
-            if (index > -1) {
-                bees.splice(index, 1);
+        this.remove = function () {
+            //remove the bee and delete its IMG
+            if (typeof bees !== 'undefined') {
+                //remove element from bees array
+                const index = bees.indexOf(this);
+                if (index > -1) {
+                    bees.splice(index, 1);
+                }
             }
-        }
-        //remove element from div
-        this.htmlElement.parentNode.removeChild(this.htmlElement);
-    };
+            //remove element from div
+            this.htmlElement.parentNode.removeChild(this.htmlElement);
+        };
 
+    }
 }
 
 function createBeeImg(wNum) {
